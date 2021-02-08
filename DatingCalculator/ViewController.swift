ba7f7e08b1fa03351d5bb8ae5004a9c64c514cfb
Omri Horowitz <horowitzomri@gmail.com>
2021-02-08 14:59:39 -0800

    Initial Commit

---
+++ DatingCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatingCalculator
+//
+//  Created by Omri Horowitz on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
